1633142a7543526ddd4c94593b1b27a769e0791c
innasukhina <innasukhina@gmail.com>
2022-01-15 14:33:27 +0200

    Редактировала

--- XUnitTestProject4/PageObject/HomePage.cs
+++ XUnitTestProject4/PageObject/HomePage.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 
 namespace XUnitTestProject4.PageObject
 {
-    class HomePage: Header
+    class HomePage : HeaderFooter
     {
-
-
         public HomePage(IWebDriver driver)
         {
             _driver = driver;
+        }
+
+        private By _clickPopularElement = By.XPath("//a[contains(text(),'Popular')]");
+        private By _clickBestSellersElement = By.XPath("//div[2]/div/ul/li[2]/a");
+        private By _fadedShrtSlvTshirtElem = By.XPath("//div[2]/div/ul/li[2]/a");
+
+        public HomePage clickPopularElement()
+        {
+            _driver.FindElement(_clickPopularElement).Click();
+            return this;
+        }
+        public HomePage clickBestSellersElement()
+        {
+            _driver.FindElement(_clickBestSellersElement).Click();
+            return this;
+        }
+        public HomePage hoverFadedShortSleevTshirt()
+        {
+            _driver.FindElement(_fadedShrtSlvTshirtElem).Click();
+            return this;
         }
     }
 }
