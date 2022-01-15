e1c39f651d20880084e8fc5683d25c051670f55c
ekaterinasugaj <shugay_k@ukr.net>
2022-01-15 14:40:24 +0200

    fix

--- XUnitTestProject4/PageObject/BlousesPage.cs
+++ XUnitTestProject4/PageObject/BlousesPage.cs
@@ -5,7 +5,7 @@ using OpenQA.Selenium;
 
 namespace XUnitTestProject4.PageObject
 {
-    class BlousesPage : Header
+    class BlousesPage : HeaderFooter
     {
         public BlousesPage(IWebDriver driver)
         {
--- XUnitTestProject4/PageObject/SingInPage.cs
+++ XUnitTestProject4/PageObject/SingInPage.cs
@@ -5,7 +5,7 @@ using OpenQA.Selenium;
 
 namespace XUnitTestProject4.PageObject
 {
-    class SignInPage : Header
+    class SignInPage : HeaderFooter
     {
         public SignInPage(IWebDriver driver)
         {
