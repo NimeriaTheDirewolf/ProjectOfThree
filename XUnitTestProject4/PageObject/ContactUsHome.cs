9b2e8c880095475268587ebf292c37c6aa47595f
Влад Прокопенко1 <nonprokopenko@gmail.com>
2022-01-16 16:41:24 +0200

    add fix

---
+++ XUnitTestProject4/PageObject/ContactUsHome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace XUnitTestProject4.PageObject
+{
+    public class ContactUsHome : HeaderFooter
+    {
+            public ContactUsHome(IWebDriver driver)
+            {
+                _driver = driver;
+            }
+    }
+}
---
+++ XUnitTestProject4/PageObject/ContactUsMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace XUnitTestProject4.PageObject
+{
+    public class ContactUsMessage : HeaderFooter
+    {
+        public ContactUsMessage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+    }
+}
