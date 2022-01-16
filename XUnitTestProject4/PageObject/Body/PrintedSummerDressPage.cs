44b7c672f00de8f02f63d5d84e20ae3621c42cc8
NimeriaTheDireWolf <95609495+NimeriaTheDirewolf@users.noreply.github.com>
2022-01-16 19:41:14 +0200

    имя доб

--- XUnitTestProject4/PageObject/Body/PrintedSummerDressPage.cs
+++ XUnitTestProject4/PageObject/Body/PrintedSummerDressPage.cs
@@ -6,7 +6,9 @@ using OpenQA.Selenium;
 namespace XUnitTestProject4.PageObject
 {
     public class PrintedSummerDressPage
-    {
+    { 
+        // Инна. 
+        
         IWebDriver _driver;
 
         public PrintedSummerDressPage(IWebDriver driver)
